aa6df3879ef686d9f2fb39896a69d6c81b56e811
Bruno Anacleto Santos de Sousa - DATAPREVRN <bruno.anacleto@dataprev.gov.br>
2019-08-15 11:34:05 -0300

    criando os componentes de restaurantes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'restaurants', component: RestaurantsComponent }
 ];
 
 @NgModule({
