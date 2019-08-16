97ac6dbc128d8c5658f9745c6e9fb4bfab31b148
Bruno Anacleto <anacleto@gmail.com>
2019-08-16 00:31:26 -0300

    tratamento de erros com o operador catch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -3,9 +3,11 @@ import { Http } from "@angular/http";
 
 import { Observable } from "rxjs/Observable"
 import "rxjs/add/operator/map"
+import "rxjs/add/operator/catch"
 
 import { Restaurant } from "./restaurant/restaurant.model";
 import { MEAT_API } from "./../app.api";
+import { ErrorHandler } from "app/app.error-handler";
 
 /**
  * Assim como a gente criou o serviço 'RestaurantService' e injetou dentro do componente 'RestaurantsComponent',
@@ -22,11 +24,12 @@ export class RestaurantService {
   // Recebendo a injeção do serviço http
   constructor(private http: Http) { }
 
-  // Consumindo a API mockada do json-server
+  // Configuração do Observable: Consumindo a API mockada do json-server
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`) // até aqui o retorno é do tipo Observable<Response> (resposta crua: status, mensagens ok/erro, corpo...)
       .map(response => response.json()) // transformando (map) o objeto Observable<Response> num array de restaurantes (Observable<Restaurant[]>)
       // A chamada http não será feita nesse momento, precisamos primeiro fazer um subscribe. Apenas depois que fizermos um subscribe é que a 
       // requisição será feita. Dessa forma, o subscribe será feito no componente 'RestaurantsComponent'.
+      .catch(ErrorHandler.errorHandler)
   }
 }
