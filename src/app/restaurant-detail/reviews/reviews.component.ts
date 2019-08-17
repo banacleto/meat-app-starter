4a73bdb936a6291cdfe28aa148031549666a20f9
Bruno Anacleto <anacleto@gmail.com>
2019-08-17 15:32:57 -0300

    implementando o componente de avaliações - pipes async e date

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -16,7 +16,11 @@ export class ReviewsComponent implements OnInit {
     private route: ActivatedRoute
   ) { }
 
+  // O subscribe não foi feito aqui, pois ficou decidido que usariamos o pipe async para tal funcão.
   ngOnInit() {
+    // O detalhe do restaurante tem o caminho /restaurants/id, então a rota que é acionada é diretamente para aquele componente.
+    // Aqui, estamos num componte filho, ou seja, é uma subrota e o parâmetro não é desse componente e sim do componente parent.
+    // Dessa forma, vamos ter que acessar esse dado através de um objeto chamado de parent da rota.
     this.reviews = this.restaurantService
       .reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
   }
