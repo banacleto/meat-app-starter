3b24440788f3ed4b63f0504c9749473b70e4b978
Bruno Anacleto <anacleto@gmail.com>
2019-08-28 16:12:01 -0300

    criando o componente de compra

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,17 +7,20 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { OrderComponent } from './order/order.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'restaurants', component: RestaurantsComponent },
-    { path: 'restaurants/:id', component: RestaurantDetailComponent,
+    {
+        path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
-            { path: '', redirectTo: 'menu', pathMatch: 'full'}, // Quando eu não informar niguém, serei redirecionado para o menu e o caminho será '*restaurants/1'
-            { path: 'menu', component: MenuComponent }, 
+            { path: '', redirectTo: 'menu', pathMatch: 'full' }, // Quando eu não informar niguém, serei redirecionado para o menu e o caminho será '*restaurants/1'
+            { path: 'menu', component: MenuComponent },
             { path: 'reviews', component: ReviewsComponent }]
-    }
+    },
+    { path: 'order', component: OrderComponent }
 ];
 
 @NgModule({
