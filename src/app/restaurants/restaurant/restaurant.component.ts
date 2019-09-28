8a3a125d63482b3ed556fb345d6492eaae4d2d04
Bruno Anacleto <anacleto@gmail.com>
2019-09-27 23:31:38 -0300

    animação com void e estilos em transições (Restaurantes)

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -7,17 +7,18 @@ import { Restaurant } from './restaurant.model';
   selector: 'mt-restaurant',
   templateUrl: './restaurant.component.html',
   animations: [
-    trigger('restaurant-appeared', [
-      state('ready', style({ opacity: 1 })),
-      transition('void => ready', [
-        style({ opacity: 0, transform: 'translate(-30px, -10px)' }),
-        animate('300ms 0s ease-in-out')
+    trigger('restaurant-appeared', [ // O nome da trigger é o nome da animação
+      state('ready', style({ opacity: 1 })), // Definindo um estado e o estilo relacionado a esse estado
+      transition('void => ready', [ // Declarando uma transição de um estado que não está na arvore de componentes para quando ele entra nela
+        style({ opacity: 0, transform: 'translate(-30px, -10px)' }), // Aplicando um estilo na transição
+        animate('300ms 0s ease-in-out') // Definindo como vai ser a nossa animação (duração, delay, tipo)
       ])
     ])
   ]
 })
 export class RestaurantComponent implements OnInit {
 
+  // O estado inicial é 'ready' porque a transição que nos interessa é de 'void => ready'
   restaurantState = 'ready'
   @Input() restaurant: Restaurant
 
