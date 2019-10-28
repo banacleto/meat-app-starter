9396b23c18b32e6df4887519e2b8895fd3e6ddc9
Bruno Anacleto Santos de Sousa - DATAPREVRN <bruno.anacleto@dataprev.gov.br>
2019-10-28 15:08:08 -0300

    utilizando zonas

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,29 +1,34 @@
 import { HttpErrorResponse } from "@angular/common/http";
-import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { ErrorHandler, Injectable, Injector, NgZone } from "@angular/core";
 import { NotificationService } from "./shared/messages/notification.service";
 import { LoginService } from "./security/login/login.service";
 
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
 
-    constructor(private notificationService: NotificationService, private injector: Injector) {
+    constructor(private notificationService: NotificationService,
+        private injector: Injector, private zone: NgZone) {
         super()
     }
 
     handleError(errorResponse: HttpErrorResponse | any) {
         if(errorResponse instanceof HttpErrorResponse) {
-            const message = errorResponse.error.message 
-            switch(errorResponse.status) {
-                case 401:
-                    this.injector.get(LoginService).handleLogin()
-                break;
-                case 403:
-                    this.notificationService.notify(message || 'Não autorizado.')
-                break;
-                case 404:
-                    this.notificationService.notify(message || 'Recurso não encontrado. Verifique console para mais detalhes.')
-                break;
-            }
+            const message = errorResponse.error.msg
+            
+            // Zona monitorada pelo Angular para corrigir o problema de falta de sincronismo da mensagem
+            this.zone.run(()=>{
+                switch(errorResponse.status) {
+                    case 401:
+                        this.injector.get(LoginService).handleLogin()
+                    break;
+                    case 403:
+                        this.notificationService.notify(message || 'Não autorizado.')
+                    break;
+                    case 404:
+                        this.notificationService.notify(message || 'Recurso não encontrado. Verifique console para mais detalhes.')
+                    break;
+                }
+            })
         }
         super.handleError(errorResponse)
     }
