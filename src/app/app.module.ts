9394f7b9a8cd6022a025d94da40d277c22ae88cc
Bruno Anacleto Santos de Sousa - DATAPREVRN <bruno.anacleto@dataprev.gov.br>
2019-10-29 09:48:17 -0300

    atualização das dependências da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
 import { ErrorHandler, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,6 +22,7 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { LoginComponent } from './security/login/login.component';
 import { SharedModule } from './shared/shared.module';
 
+registerLocaleData(localePt, 'pt')
 
 @NgModule({
   declarations: [
@@ -52,10 +55,10 @@ import { SharedModule } from './shared/shared.module';
      * Este tipo de notação é um modelo extendido da declaração de providers acima. Quando usamos RestaurantServive, 
      * na verdade estamos dizendo: { provide: RestaurantService, useClass: RestaurantService }
      * Mas para não ficar muito verboso, usamos a sintaxe reduzida. O que estamos fazendo com o LOCALE_ID é uma
-     * sintaxe mais extendida, onde sempre que um componente pedir o token LOCALE_ID, ele vai receber o valor 'pt-BR'.
+     * sintaxe mais extendida, onde sempre que um componente pedir o token LOCALE_ID, ele vai receber o valor 'pt'.
      * Observação: ver os imports nos arquivos package.json e polyfills.js
      */
-    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: LOCALE_ID, useValue: 'pt' },
     { provide: ErrorHandler, useClass: ApplicationErrorHandler }
   ],
   bootstrap: [AppComponent]
